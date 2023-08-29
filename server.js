6d8b914d6c81e262dde290a21fd287ec750c1d98
Anothai <anothai.soon@northbkk.ac.th>
2023-08-29 12:44:01 +0700

    updated

---
+++ server.js
@@ -0,0 +1,17 @@
+express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(express.json())
+
+app.use(express.urlencoded({extend: true}))
+
+app.get("/", (req, res) => {
+    res.json({meassage: "Welcome to ITD102 application"})
+})
+
+require("./app/routes/tutorial.routes.js")(app)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
